42ce075117b17cd1522f293c974b6d1d10373032
Alex Lu <alexlu876@gmail.com>
2017-01-06 15:35:18 -0500

    b

---
+++ AkhmedovDungeon.java
@@ -0,0 +1,59 @@
+import java.io.*;
+public class AkhmedovDungeon{
+    
+    String name;
+    String Class;
+    String[] classes = {"warrior", "mage", "hunter"};
+    BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args){
+	AkhmedovDungeon AD = new AkhmedovDungeon();
+	System.out.println("Welcome to Mr. Akhmedov's Dungeon");
+	System.out.println("What is your name, adventurer?");
+	AD.initialize();
+	AD.printClass();
+	
+    }
+
+    public void initialize(){
+	try{
+	    name = BR.readLine();
+	    if(name.length() == 0){
+		throw new Exception();
+	    }
+	    System.out.println("Welcome, " + name);
+	    return;
+	}
+	catch(Exception err){
+	    System.out.println("Please put in at least one character");
+	}
+	return initialize();
+    }
+
+    private void printClass(){
+	String x = name + ", would you like to become";
+	for(int i = 0; i < classes.length; i++){
+	    if(i + 1 == classes.length){
+		x += " or";
+	    }
+	    x += " a " + classes[i] + ",";
+	}
+        x = x.substring(0, x.length() - 1) + "?";
+	System.out.println(x);
+	try{
+	    int temp = Integer.parseInt(BR.readLine());
+	    if(temp > classes.length - 1){
+		throw new Exception();
+	    }
+	}
+        catch(Exception err){
+	    System.out.println("Please input a valid number");
+	    return printClass();
+	}
+	Class = classes[temp];
+	
+    }
+           
+	   
+}
+	
+    
