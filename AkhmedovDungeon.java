540cfe9a2493ddc2a8e160c56a157eae7a8c497a
Alex Lu <alexlu876@gmail.com>
2017-01-09 15:07:20 -0500

    almost working

--- AkhmedovDungeon.java
+++ AkhmedovDungeon.java
@@ -25,8 +25,9 @@ public class AkhmedovDungeon{
 	}
 	catch(Exception err){
 	    System.out.println("Please put in at least one character");
+	    initialize();
 	}
-	return initialize();
+	return;
     }
 
     private void printClass(){
@@ -39,15 +40,16 @@ public class AkhmedovDungeon{
 	}
         x = x.substring(0, x.length() - 1) + "?";
 	System.out.println(x);
+	int temp;
 	try{
-	    int temp = Integer.parseInt(BR.readLine());
+	    temp = Integer.parseInt(BR.readLine());
 	    if(temp > classes.length - 1){
 		throw new Exception();
 	    }
 	}
         catch(Exception err){
 	    System.out.println("Please input a valid number");
-	    return printClass();
+	    printClass();
 	}
 	Class = classes[temp];
 	
